8fe38ca209975e251f6b3e9262ee89b145e4f941
zeeket <3qvm@tutanota.com>
2020-11-16 21:05:00 +0200

    fields, accessors, toString formatting

--- src/main/java/ohtu/Player.java
+++ src/main/java/ohtu/Player.java
@@ -2,7 +2,57 @@
 package ohtu;
 
 public class Player {
-    private String name;
+    private String name, nationality, team;
+    private int assists,goals,penalties,games;
+
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setGoals(int goals) {
+        this.goals= goals;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setPenalties(int penalties) {
+        this.penalties= penalties;
+    }
+
+    public int getPenalties() {
+        return penalties;
+    }
+
+    public void setGames(int games) {
+        this.games= games;
+    }
+
+    public int getGames() {
+        return games;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -14,7 +64,8 @@ public class Player {
 
     @Override
     public String toString() {
-        return name;
+        String ret = getName() + " team " + getTeam() + " goals " + String.valueOf(getGoals()) + " assists " + String.valueOf(getAssists());
+        return ret;
     }
-      
+
 }
