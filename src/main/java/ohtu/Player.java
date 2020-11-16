eb11c80cb3dd126cfd020cc6fb6c044f0d6e9606
zeeket <3qvm@tutanota.com>
2020-11-16 12:57:17 +0200

    haettu koodit

---
+++ src/main/java/ohtu/Player.java
@@ -0,0 +1,20 @@
+
+package ohtu;
+
+public class Player {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+      
+}
