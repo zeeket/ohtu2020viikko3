777240c71d995c21d8d9d695d89af37c395fa15f
zeeket <3qvm@tutanota.com>
2020-11-16 15:58:18 +0200

    it runs

--- src/main/java/ohtu/Main.java
+++ src/main/java/ohtu/Main.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ohtu;
 
-/**
- *
- * @author mluukkai
- */
+import com.google.gson.Gson;
+import java.io.IOException;
+import org.apache.http.client.fluent.Request;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
+        
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+                
+        System.out.println("json-muotoinen data:");
+        System.out.println( bodyText );
+
+        Gson mapper = new Gson();
+        Player[] players = mapper.fromJson(bodyText, Player[].class);
+        
+        System.out.println("Oliot:");
+        for (Player player : players) {
+            System.out.println(player);
+        }   
+    }
+  
 }
